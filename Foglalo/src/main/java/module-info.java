ab7cc1a6710e167cea9ceb41460e2b17c80289d2
Kiss Márton <marci20041103@hotmail.com>
2024-03-10 17:28:34 +0100

    added docs and removed the javax project

--- Foglalo/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.foglalo {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires com.dlsc.formsfx;
-
-    opens com.example.foglalo to javafx.fxml;
-    exports com.example.foglalo;
-}
